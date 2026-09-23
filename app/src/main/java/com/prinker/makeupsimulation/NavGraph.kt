496a32aee9692f8636c4d3b7819424c3618e19df
hydong <donghwayeong@gmail.com>
2026-09-23 15:59:56 +0900

    init navigation

---
+++ app/src/main/java/com/prinker/makeupsimulation/NavGraph.kt
@@ -0,0 +1,24 @@
+package com.prinker.makeupsimulation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavGraph(
+    navController: NavHostController = rememberNavController(),
+) {
+    Scaffold { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "home",
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            
+        }
+    }
+}
